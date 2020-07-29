e6ecc029526738b10ec456047d22ab509591abb9
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-07-29 09:47:36 -0500

    Update dbConnection.js

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,20 +1,20 @@
 const mysql = require("mysql");
 
-module.exports = () => {
-  return mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "123456",
-    database: "bdd",
-  });
-};
-
-
 // module.exports = () => {
 //   return mysql.createConnection({
-//     host: "85.10.205.173",
-//     user: "leonelparrales22",
-//     password: "Pinguino2233",
-//     database: "adminpro",
+//     host: "localhost",
+//     user: "root",
+//     password: "123456",
+//     database: "bdd",
 //   });
 // };
+
+
+module.exports = () => {
+  return mysql.createConnection({
+    host: "85.10.205.173",
+    user: "leonelparrales22",
+    password: "Pinguino2233",
+    database: "adminpro",
+  });
+};
